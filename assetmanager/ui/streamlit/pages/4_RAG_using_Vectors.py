4d72e0ac1fe6975e1690b77af0d1f2f7b91d096c
Ezhil <3431406+ezhilvendhan@users.noreply.github.com>
2024-04-04 08:00:46 +0800

    Update questions

--- assetmanager/ui/streamlit/pages/4_RAG_using_Vectors.py
+++ assetmanager/ui/streamlit/pages/4_RAG_using_Vectors.py
@@ -98,9 +98,9 @@ st.markdown("""
     <th colspan="3">Sample Questions to try out</th>
   </tr>
   <tr>
-    <td>Which asset managers have investments in outside USA? Explain with evidence</td>
-    <td>Which companies and asset managers are vulnerable to chip shortage?</td>
     <td>Which companies and asset managers are most affected during covid?</td>
+    <td>Which companies and asset managers are vulnerable to chip shortage?</td>
+    <td>Which asset managers have investments in outside USA? Explain with evidence</td>
   </tr>
   <tr>
     <td>Which asset managers are exposed to defence industries?</td>
